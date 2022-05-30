fa4efa55f658ce9a0302651efcbda9989c030b5c
YIN <yin@YINdeMacBook-Air.local>
2022-05-30 15:25:29 +0800

    Practice Finished.

--- app.js
+++ app.js
@@ -1,140 +1,145 @@
-
 // ============use the mongoose ===============
 
 
 const mongoose = require('mongoose');
 // if doesn't exist , create a brand new one
-mongoose.connect('mongodb://localhost:27017/fruitsDB',{useNewUrlParser:true});
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  age : Number
+mongoose.connect('mongodb://localhost:27017/fruitsDB', {
+  useNewUrlParser: true
 });
 
+
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating : Number,
+  name: {
+    type: String,
+    required: [true, "Please check the entry,there is no name!"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
-const Person = mongoose.model("Person",personSchema);
-const Fruit = mongoose.model("Fruit",fruitSchema);
-
-const person = new Person({
-  name: "John",
-  age : 35
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
 });
+
+
+const Person = mongoose.model("Person", personSchema);
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
 
 // person.save();
 
+//delete many
+// Person.deleteMany({name:"John"},function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("successfully deleted the document!");
+//   }
+// });
+
+
 const kiwi = new Fruit({
-  name:"Kiwi",
-  rating:10,
-  review:"Awesome!"
+  name: "Kiwi",
+  rating: 10,
+  review: "Awesome!"
 });
 // kiwi.save();
 
 const organge = new Fruit({
-  name:"Orange",
-  rating:7,
-  review:"Good."
+  name: "Orange",
+  rating: 7,
+  review: "Good."
 });
 const banana = new Fruit({
-  name:"Banana",
-  rating:9,
-  review:"Soft."
+  name: "Banana",
+  rating: 10,
+  review: "Soft."
 });
 
-Fruit.insertMany([organge,banana],function(err){
-  if (err){
+
+const person = new Person({
+  name: "John",
+  age: 23
+});
+
+Person.updateOne({
+  name:"John"
+}, {
+  favoriteFruit: banana
+}, function(err) {
+  if (err) {
     console.log(err);
-  }else{
-    console.log("successfully saved all the fruits to fruitsDB");
+  } else {
+    console.log("successfully updated the document");
   }
 });
 
-
-
-
-
-
-
-// ==========the original driver =============
-// const MongoClient = require("mongodb").MongoClient;
-// const assert = require("assert"); // for testing
-//
-// // Connection URI
-// const uri =
-//   "mongodb://localhost:27017";
-// // database name
-// const dbName = "fruitsDB"
-// // Create a new MongoClient
-// const client = new MongoClient(uri);
-//
-//
-// client.connect(function(err) {
-//   assert.equal(null, err);
-//   console.log("connected successfully to server");
-//
-//   const db = client.db(dbName);
-//
-//   // insert document
-//   // insertDocuments(db, function() {
-//   //   client.close(); // once the inset document down, close the database clients
-//   // });
-//
-//   // query documents
-//   findDocuments(db,function(){
-//     client.close();
-//   });
-//
-//   // client.close();
+// person.save(function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("successfully saved the document");
+//   }
 // });
-//
-// // insert operations
-// const insertDocuments = function(db, callback) {
-//   const fruits = db.collection("fruits");
-//   // create a document to insert
-//   const docs = [{
-//       name: "Apple",
-//       score: 8,
-//       review: "Great fruit"
-//     },
-//     {
-//       name: "Orange",
-//       score: 6,
-//       review: "Kinda sour"
-//     },
-//     {
-//       name: "Banana",
-//       score: 9,
-//       review: "Great stuff!"
-//     }
-//   ];
-//   // this option prevents additional documents from being inserted if one fails
-//   const options = {
-//     ordered: true
-//   };
-//   fruits.insertMany(docs, options, function(err, result) {
-//     // for validation
-//     assert.equal(err, null), // make sure no errors.
-//       assert.equal(3, result.insertedCount); // ensure has three inserts
-//     // assert.equal(3, result.ops.length);
-//     //
-//     console.log("insert 3 columns into the collection");
-//     callback(result);
-//   });
-// }
-//
-// // query operations
-// const findDocuments = function(db, callback) {
-//   // get collection
-//   const collection = db.collection("fruits");
-//   // find some documents
-//   collection.find({}).toArray(function(err,fruits){
-//     assert.equal(err,null);
-//     console.log("Found the following records");
-//     console.log(fruits);
-//     callback(fruits);
-//   });
-// }
+
+// fruit.save(function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("save successfully1");
+//   }
+// });
+
+//update
+// Fruit.updateOne({
+//   _id: "6294663554298b8989eefd88"
+// }, {
+//   name: "Tomato"
+// }, function(err) {
+//   if (err) {
+//     console.log(err);
+//   }else{
+//     console.log("successfully updated the document");
+//   }
+// });
+
+// remove
+// Fruit.deleteOne({
+//   name:"Pears"
+// }, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("successfully deleted");
+//   }
+// });
+
+// // Insert fruits operation
+// Fruit.insertMany([organge,banana],function(err){
+//   if (err){
+//     console.log(err);
+//   }else{
+//     console.log("successfully saved all the fruits to fruitsDB");
+//   }
+// });
+
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    // close the connection of mongodb
+    // Notice : once finish the operation close the connection
+
+    fruits.forEach(function(fruit) {
+      console.log(fruit);
+    });
+    mongoose.connection.close();
+    // console.log(fruits); // log in format of javascript
+  }
+});
